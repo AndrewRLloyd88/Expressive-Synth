16963178e818cba41a6d307383b5a9d23070933b
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-19 12:14:11 -0800

    include tone.js and do hello world with tone

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import * as Tone from 'tone';
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  return <h1>Expressive Synth</h1>;
+  const synth = new Tone.Synth().toDestination();
+  const handleClick = () => {
+    synth.triggerAttackRelease('C4', '8n');
+  };
+
+  return (
+    <div>
+      <h1>Expressive Synth</h1>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
 }
 
 export default App;
