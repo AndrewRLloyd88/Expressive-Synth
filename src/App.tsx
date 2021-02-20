75475c7a34e411c643e062f379bfe59c4135feb7
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-19 17:36:49 -0800

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,36 @@ import * as Tone from 'tone';
 interface AppProps {}
 
 function App({}: AppProps) {
-  //define new keyboard
+  //define new keyboard from class Keyboard
   const keyboard = new Keyboard();
-  // console.log(keyboard);
-  const synth = new Tone.Synth().toDestination();
+
+  // patch
+  const synth = new Tone.PolySynth().toDestination();
 
   //every time a button is pressed pass in the keys value and play that note
   const handleClick = (id: string) => {
-    // console.log(id);
+    //triggers a note and an attack release time.
     synth.triggerAttackRelease(`${id}`, '8n');
   };
 
+  //map out our keyboard with the appropriate keys
   return (
     <div>
       <h1>Expressive Synth</h1>
-      {keyboard.keys.map((key, id) => {
-        return (
-          <button
-            key={id}
-            onClick={() => {
-              handleClick(key);
-            }}
-          >
-            {key}
-          </button>
-        );
-      })}
+      <div className="keyboard">
+        {keyboard.keys.map((key, id) => {
+          return (
+            <button
+              key={id}
+              onClick={() => {
+                handleClick(key);
+              }}
+            >
+              <p>{key}</p>
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
