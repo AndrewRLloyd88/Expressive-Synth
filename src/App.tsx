185f70076e4b4b143581e0bfe2a58b00e5881967
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-19 16:11:41 -0800

    map out C4-B4 keyboard from keyboard class

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Keyboard } from './Keyboard';
 import * as Tone from 'tone';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  //define new keyboard
+  const keyboard = new Keyboard();
+  // console.log(keyboard);
   const synth = new Tone.Synth().toDestination();
-  const handleClick = () => {
-    synth.triggerAttackRelease('C4', '8n');
+
+  //every time a button is pressed pass in the keys value and play that note
+  const handleClick = (id: string) => {
+    // console.log(id);
+    synth.triggerAttackRelease(`${id}`, '8n');
   };
 
   return (
     <div>
       <h1>Expressive Synth</h1>
-      <button onClick={handleClick}>Click me</button>
+      {keyboard.keys.map((key, id) => {
+        return (
+          <button
+            key={id}
+            onClick={() => {
+              handleClick(key);
+            }}
+          >
+            {key}
+          </button>
+        );
+      })}
     </div>
   );
 }
