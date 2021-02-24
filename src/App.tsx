db12e28782d5c5c4662b42c09d0fdd4a6bd9a2f4
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-23 17:06:40 -0800

    update app to accomodate attack

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,51 @@ import * as Tone from 'tone';
 interface AppProps {}
 
 function App({}: AppProps) {
+  const [currentSynthSettings, setCurrentSynthSettings] = useState({
+    type: 'triangle21',
+    attack: 5,
+    decay: 5,
+    sustain: 0.4,
+    release: 4,
+  });
   //define new keyboard from class Keyboard
   const keyboard = new Keyboard();
 
   // patch
   const synth = new Tone.PolySynth(Tone.Synth, {
     oscillator: {
-      type: 'triangle21',
+      type: currentSynthSettings.type as 'custom',
     },
     envelope: {
-      attack: 5,
-      decay: 1,
-      sustain: 0.4,
-      release: 4,
+      attack: currentSynthSettings.attack,
+      decay: currentSynthSettings.decay,
+      sustain: currentSynthSettings.sustain,
+      release: currentSynthSettings.release,
     },
   }).toDestination();
 
   const now = Tone.now();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.id);
+    console.log(event.target.value);
+    const newAttack = parseInt(event.target.value);
+    setCurrentSynthSettings((prevState) => {
+      return { ...prevState, attack: newAttack };
+    });
+  };
 
   //every time a button is pressed pass in the keys value and play that note
   const handleClick = (id: string) => {
     //triggers a note and an attack release time.
     Tone.context.resume().then(() => {
       console.log(synth);
-      synth.triggerAttack(`${id}`, now);
+      synth.triggerAttackRelease(`${id}`, now);
     });
   };
 
   const release = (id: string) => {
-    synth.triggerRelease(`${id}`, now);
+    synth.triggerRelease(`${id}`);
   };
 
   //map out our keyboard with the appropriate keys
@@ -59,6 +75,21 @@ function App({}: AppProps) {
             ></button>
           );
         })}
+      </div>
+      <div>
+        <label htmlFor="Attack">
+          Attack {currentSynthSettings.attack}
+          <input
+            min={0}
+            type="range"
+            id={'attack'}
+            onChange={(event) => {
+              handleChange(event);
+              console.log(currentSynthSettings.attack);
+            }}
+            value={currentSynthSettings.attack}
+          ></input>
+        </label>
       </div>
     </div>
   );
